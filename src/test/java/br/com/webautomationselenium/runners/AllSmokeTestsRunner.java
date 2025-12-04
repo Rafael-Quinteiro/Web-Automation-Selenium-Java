05193b116ec2df38e8f7fcd828f9bf3d24c456ce
Rafael de Oliveira Quinteiro <967835@lojasrenner.com.br>
2025-12-04 13:52:17 -0300

    Ajuste na documentação das classes runners

--- src/test/java/br/com/webautomationselenium/runners/AllSmokeTestsRunner.java
+++ src/test/java/br/com/webautomationselenium/runners/AllSmokeTestsRunner.java
@@ -7,12 +7,19 @@ import org.junit.runners.Suite;
 import br.com.webautomationselenium.suits.AllSmokeTests;
 import br.com.webautomationselenium.tests.OrderTest;
 
+/**
+ * Test runner responsible for executing all tests
+ * classified as Smoke Tests.
+ * 
+ * It uses the JUnit Categories mechanism to filter
+ * and run only the test classes annotated with
+ * the {@link AllSmokeTests} category.
+ */
 @RunWith(Categories.class)
-
 @Suite.SuiteClasses({
-        OrderTest.class,
+        OrderTest.class
 })
 @Categories.IncludeCategory(AllSmokeTests.class)
 public class AllSmokeTestsRunner {
-
+    // This class is used only as an orchestrator for Smoke test execution
 }
--- src/test/java/br/com/webautomationselenium/suits/AllSmokeTests.java
+++ src/test/java/br/com/webautomationselenium/suits/AllSmokeTests.java
@@ -1,5 +1,14 @@
 package br.com.webautomationselenium.suits;
 
+/**
+ * Marker interface used to classify Smoke Tests.
+ * 
+ * Test classes or test methods annotated with this interface
+ * will be included when executing the Smoke Test Suite.
+ * 
+ * It is used together with JUnit Categories and the
+ * {@code AllSmokeTestsRunner} class.
+ */
 public interface AllSmokeTests {
-    
+    // Marker interface for Smoke Test category
 }
